0e053e59e576e67b061e480148151cb120a3c266
Ralph Stout <ralph.stout14@icloud.com>
2023-05-18 19:58:47 -0500

    Added Projects to the project page with a stylish effect

--- src/components/ProjectsPage/Projects.jsx
+++ src/components/ProjectsPage/Projects.jsx
@@ -1,9 +1,34 @@
 import './Projects.css';
+import StalkerPreview from "../../Assets/StalkerPreview.jpeg";
+import ToDoList from "../../Assets/ToDoList.png";
 
 function Projects(){
     return (
         <div className='Projects-Container' id='Projects-Container'>
             <h1>Projects</h1>
+            <div className="cardcontainer">
+                <div className="cards">
+                    <img class="zoom" src={StalkerPreview} alt="StalkerPreview"></img>
+                    <div className="Content fade">
+                        <div className="title">Stalker Social App</div>
+                        Stalker the social app to become friends with strangers!
+                        This app was created so that you can chat with others around
+                        you, and if both parties are enjoying their chat they can
+                        share each others location.
+                    </div>
+                </div>
+                <div className="cards">
+                    <img class="zoom" src={ToDoList} alt="StalkerPreview"></img>
+                    <div className="Content fade">
+                        <div className="title">ToDoList</div>
+                        A simple to do List that allows you to track task you
+                        have set for the day. If you need to remind yourself about
+                        something just hop on the website and add a task to your
+                        list. Its that easy!
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
