04da5e18b5533ad6e1c6bdabfac16f9e3a936703
Ralph Stout <ralph.stout14@icloud.com>
2023-05-24 13:43:49 -0500

    made project pages responsive, added my third project which is in progress

--- src/components/ProjectsPage/Projects.jsx
+++ src/components/ProjectsPage/Projects.jsx
@@ -1,6 +1,7 @@
 import './Projects.css';
 import StalkerPreview from "../../Assets/StalkerPreview.jpeg";
 import ToDoList from "../../Assets/ToDoList.png";
+import InProgress from "../../Assets/Code.jpg";
 
 function Projects(){
     return (
@@ -18,13 +19,23 @@ function Projects(){
                     </div>
                 </div>
                 <div className="cards">
-                    <img class="zoom" src={ToDoList} alt="StalkerPreview"></img>
+                    <img class="zoom" src={ToDoList} alt="ToDoList"></img>
                     <div className="Content fade">
                         <div className="title">ToDoList</div>
                         A simple to do List that allows you to track task you
                         have set for the day. If you need to remind yourself about
                         something just hop on the website and add a task to your
                         list. Its that easy!
+                    </div>
+                </div>
+                <div className="cards">
+                    <img class="zoom" src={InProgress} alt="Project In Progress"></img>
+                    <div className="Content fade">
+                        <div className="title">The Flower Slab In-Progress</div>
+                        A Website for a small florist. This website will allow
+                        customers to create and purchase a unique bouquet of flowers
+                        of the florists existing inventory. This project will use the
+                        Stripe payment API.
                     </div>
                 </div>
 
